b1c806776ce87d96740fd2cdd49fc3a8bb461d7b
Lauri <116688165+laurindoisaac@users.noreply.github.com>
2025-08-10 01:04:10 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/frauddetection/main.go
@@ -0,0 +1,23 @@
+// cmd/frauddetection/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "frauddetection/internal/frauddetection"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := frauddetection.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
